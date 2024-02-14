732e7134a2783affea70620559f09645cad52453
Axel Ibo <axelyvesi2@gmail.com>
2024-02-14 11:49:02 +0100

    Test

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function HelloWorld(name) {
+    if (name === void 0) { name = "axel"; }
+    console.log("Hello ".concat(name));
+}
+exports.default = HelloWorld;
+HelloWorld();
---
+++ index.ts
@@ -0,0 +1,5 @@
+export default function HelloWorld(name: string = "axel") {
+    console.log(`Hello ${name}`)
+}
+
+HelloWorld()
